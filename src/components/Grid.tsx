7dd2357e685af9f8171f1b0812ee9eb38312ad2b
savelyevresearch <savelyevresearch@protonmail.com>
2024-06-01 23:13:35 +0600

    The Bento Grid section is completed

---
+++ src/components/Grid.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+type Props = {};
+
+const Grid = (props: Props) => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
