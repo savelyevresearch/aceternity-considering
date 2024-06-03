c7d7f3a49e2bf6256accb6efacb54be3d69af954
savelyevresearch <savelyevresearch@protonmail.com>
2024-06-03 22:10:05 +0600

    The Experience section is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import RecentProjects from "../components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
